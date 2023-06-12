78195dcc4338c7d5f6283eb7e9b02ac60fa63354
Lee Martin <hello@leejohnmartin.co.uk>
2023-06-12 15:13:15 +0100

    Tweak: Prevent unauth access to non-auth roots

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,12 @@
+import { redirect } from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
+
+export const load: LayoutServerLoad = async (event) => {
+  const session = await event.locals.getSession();
+  if (!session && !event.url.pathname.startsWith('/auth')) {
+    throw redirect(302, '/auth/signin');
+  }
+  return {
+    session
+  };
+};
